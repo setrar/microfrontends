9b94c4e603f4e520eb46e70c3c15d28c73fc6fd4
u0000000021 <u0000000021@uontario.ca>
2021-05-09 11:19:50 -0400

    .

---
+++ multiple-elm-apps-on-a-single-page/src/Blue.elm
@@ -0,0 +1,7 @@
+-- Elm 0.19.0
+module Blue exposing (main)
+
+import Html exposing (..)
+
+main =
+  div [] [ text "Hello from Blue!" ]
---
+++ multiple-elm-apps-on-a-single-page/src/Red.elm
@@ -0,0 +1,8 @@
+-- Elm 0.19.0  
+
+module Red exposing (main)
+
+import Html exposing (..)
+
+main =
+  div [] [ text "Hello from Red!" ]
